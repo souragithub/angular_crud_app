2c4a325cda5fbf5d2a5bfdcea4a48b3ac3efadc3
souragithub <sourabaral2002@gmail.com>
2025-10-23 10:04:37 +0530

    Data Showing in Table

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,54 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UserModel } from './model/UserModel';
 
 
 @Component({
   selector: 'app-root',
-  imports: [ ],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  
+
+  userForm : FormGroup = new FormGroup({});
+
+  userObj : UserModel = new UserModel();
+
+  userList : UserModel [] = [];
+
+  constructor(){
+    this.createForm();
+     const data = localStorage.getItem("UserData");
+      if(data != null){
+        const parseData = JSON.parse(data);
+        this.userList = parseData;
+      }
+  }
+
+  createForm(){
+    this.userForm = new FormGroup({
+      userId : new FormControl(this.userObj.userId),
+      name  : new FormControl(this.userObj.name),
+      city  : new FormControl(this.userObj.city),
+      address  : new FormControl(this.userObj.address),
+      email  : new FormControl(this.userObj.email),
+      pin  : new FormControl(this.userObj.pin),
+    })
+
+    
+  }
+    onSave(){
+      debugger;
+      const data = localStorage.getItem("UserData");
+      if(data != null){
+        const parseData = JSON.parse(data);
+        this.userForm.controls['userId'].setValue(parseData.length + 1);
+        this.userList.unshift(this.userForm.value);
+      }
+      else{
+        this.userList.unshift(this.userForm.value);
+      }
+      localStorage.setItem("UserData",JSON.stringify(this.userList));
+    }
 }
